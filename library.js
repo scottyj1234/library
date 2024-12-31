6b5bc29530061d2dc3893cad83b4566e0cc77400
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-12-31 13:22:50 -0600

    Add table view for books in library

--- library.js
+++ library.js
@@ -15,3 +15,33 @@ function addBookToLibrary(title, author, numberOfPages, isRead) {
   const newBook = new Book(title, author, numberOfPages, isRead);
   myLibrary.push(newBook);
 }
+
+function showLibraryInfo() {
+  const libraryTable = document.querySelector(".my-library") || document.createElement('table');
+  libraryTable.innerHTML = "";
+
+  if (myLibrary.length < 1) return;
+
+  const bookProperties = Object.keys(myLibrary[0]);
+
+  libraryTable.className = "my-library";
+  const libraryTableHeaderRow = document.createElement('tr');
+  for (let headerText in bookProperties) {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = headerText;
+    libraryTableHeaderRow.appendChild(headerCell);
+  }
+  libraryTable.appendChild(libraryTable);
+
+  for (let book in myLibrary) {
+    const bookRow = document.createElement('tr');
+    for (let key in bookProperties) {
+      const contentCell = document.createElement('td');
+      contentCell.textContent = book[key];
+      bookRow.appendChild(contentCell)
+    }
+    libraryTable.appendChild(bookRow);
+  }
+
+  document.appendChild(libraryTable);
+}
